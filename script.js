02ffef4624d61b087db667d561ced5269a6216cb
yogesh <yogesh.yogesh.mec20@iitbhu.ac.in>
2024-05-13 10:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function isSameType(v1, v2) {
-  return v1===v2;
+  if(typeof(v1)==typeof(v2))return true;
+	else return false;
+
 
 	
 }
 
 // do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
